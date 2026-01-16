097049099bf31af78da247feb7fa653ba81b8127
Sahil Lathiya <sahillathiya9@gmail.com>
2026-01-16 13:37:34 +0000

    Update portfolio changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
       <div className="container">
         <h2 className="section-title">Contact</h2>
         <div className="card" style={{textAlign : 'center'}}>
-          <p>Email: sahillathiya9@gmail.com</p>
+          <p>Email: sahillathiya14@gmail.com</p>
           <p>Phone: (+44) 7823914975 </p>
           <p>Address: London, UK </p>
         </div>
